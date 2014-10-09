cade196cc484b25ff34ede5c5f9ebe5ee5913e8f
Ronen Babayoff <ronen@lavaina.com>
2014-10-08 20:19:48 -0400

    Remove unused files

--- server.js
+++
@@ -1,6 +0,0 @@
-sinon = Npm.require("sinon");
-sinonChai = Npm.require("sinon-chai");
-// If the chai package is installed use it
-if( Package['spacejamio:chai']){
-  Package['spacejamio:chai'].chai.use(sinonChai)
-}
